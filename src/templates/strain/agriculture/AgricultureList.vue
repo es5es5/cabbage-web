29ac5703850eaf46b99a6f78eea1c422d1d2f24a
es5es5 <es5e5s@nate.com>
2022-05-09 00:29:41 +0900

    Search init

--- src/templates/strain/agriculture/AgricultureList.vue
+++ src/templates/strain/agriculture/AgricultureList.vue
@@ -22,7 +22,7 @@
       <button type="button" class="btn primary" @click="showModalAgricultureCreate">등록</button>
       <!-- <button type="button" class="btn">엑셀다운로드</button> -->
     </div>
-    <div class="table_wrap table-hover">
+    <div class="table_wrap table-hover table_wrap-scoll-y">
       <table>
         <caption>농업 균주 리스트</caption>
         <colgroup>
@@ -36,49 +36,53 @@
           <col style="width: 10rem;">
           <col style="width: 10rem;">
           <col style="width: 10rem;">
-          <col style="width: 10rem;">
-          <col style="width: 10rem;">
-          <col style="width: 10rem;">
-          <col style="width: 10rem;">
         </colgroup>
         <thead>
           <tr>
-            <th scope="col">분류번호</th>
             <th scope="col">균종</th>
             <th scope="col">균주번호</th>
             <th scope="col">Origin</th>
             <th scope="col">확보일</th>
-            <th scope="col">기탁여부</th>
             <th scope="col">기탁장소</th>
             <th scope="col">보관장소</th>
             <th scope="col">stock 개수</th>
             <th scope="col">현재 stock</th>
             <th scope="col">활성테스트</th>
             <th scope="col">특허</th>
-            <th scope="col">특허내용</th>
-            <th scope="col">메모</th>
           </tr>
         </thead>
-        <tbody>
-          <tr v-for="(item, index) in _contents" :key="index" @click="showModalAgricultureUpdate(item.id)">
-            <td>{{ item.분류번호 }}</td>
-            <td>{{ item.균종 }}</td>
-            <td>{{ item.균주번호 }}</td>
-            <td>{{ item.Origin }}</td>
-            <td>{{ item.확보일 }}</td>
-            <td>{{ item.기탁여부 ? 'Y' : 'N' }}</td>
-            <td>{{ get장소(item.기탁장소) }}</td>
-            <td>{{ get장소(item.보관장소) }}</td>
-            <td>{{ item.stock갯수 }}</td>
-            <td>{{ item.현재stock }}</td>
-            <td>{{ item.활성테스트 }}</td>
-            <td>{{ item.특허 }}</td>
-            <td>{{ item.특허내용 }}</td>
-            <td>{{ item.메모 }}</td>
-          </tr>
-          <no-data-message :list="_contents" :colspan="14"></no-data-message>
-        </tbody>
       </table>
+      <div class="table_scroll">
+        <table>
+          <colgroup>
+            <col style="width: 10rem;">
+            <col style="width: 10rem;">
+            <col style="width: 10rem;">
+            <col style="width: 10rem;">
+            <col style="width: 10rem;">
+            <col style="width: 10rem;">
+            <col style="width: 10rem;">
+            <col style="width: 10rem;">
+            <col style="width: 10rem;">
+            <col style="width: 10rem;">
+          </colgroup>
+          <tbody>
+            <tr v-for="(item, index) in _contents" :key="index" @click="showModalAgricultureUpdate(item.id)">
+              <td>{{ item.균종 }}</td>
+              <td>{{ item.균주번호 }}</td>
+              <td>{{ item.Origin }}</td>
+              <td>{{ item.확보일 }}</td>
+              <td>{{ get장소(item.기탁장소) }}</td>
+              <td>{{ get장소(item.보관장소) }}</td>
+              <td>{{ item.stock갯수 }}</td>
+              <td>{{ item.현재stock }}</td>
+              <td>{{ item.활성테스트 }}</td>
+              <td>{{ item.특허 }}</td>
+            </tr>
+            <no-data-message :list="_contents" :colspan="10"></no-data-message>
+          </tbody>
+        </table>
+      </div>
       <!-- <Pagination
         :totalElement="parseInt(contents.length)"
         :activePage="searchForm.pageIndex"
