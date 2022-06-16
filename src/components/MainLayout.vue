1e73ba9a1c38ec56b10410c715ba5155a237ea48
es5es5 <es5e5s@nate.com>
2022-06-16 15:44:48 +0900

    Auth init in MainLayout

--- src/components/MainLayout.vue
+++ src/components/MainLayout.vue
@@ -14,9 +14,39 @@ import Header from '@/components/Header'
 
 export default {
   name: 'MainLayout',
+  created () {
+    this.collectionList.forEach(async (item) => {
+      this.$store.dispatch(`manage/set${item}`, item)
+    })
+    this.$axios.defaults.headers.common.Authorization = `Bearer ${this.$cookies.get('accessToken')}`
+    this.getUserProfile()
+  },
+  data () {
+    return {
+      collectionList: [
+        '장소_관리',
+        'Origin_관리',
+        '균종_관리',
+      ]
+    }
+  },
   components: {
     SideBar,
     Header
+  },
+  methods: {
+    getUserProfile () {
+      // if (!this.$cookies.get('accessToken')) return
+      const apiURL = `${this.ENV_CUOME}/auth/profile`
+      const data = {}
+      this.$axios({
+        method: 'get',
+        url: apiURL,
+        data
+      }).then(result => {
+        this.$store.commit('user/setUser', result.data)
+      })
+    },
   }
 }
 </script>
